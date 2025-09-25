f6bec1db0dab372b0278c82fc2c651823da4fc43
rasull7452 <rasullhossein250@gmail.com>
2025-09-25 16:51:48 +0200

    Task 2

--- Task 2/Main.java
+++ Task 2/Main.java
@@ -1,4 +1,3 @@
-// 2.c
 import java.util.Scanner;
 
 // 2.a
@@ -7,7 +6,27 @@ public class Main{
 	// 2.b
 	public static void main(String[] args){
 
-		System.out.println("Please type your name")
+		System.out.println("Please type your name");
+
+		// 2.c
+		Scanner scanner = new Scanner(System.in);
+
+		// 2.d
+		String name = scanner.nextLine();
+
+		// 2.e
+		System.out.println("Hello " + name + " Please type your age");
+
+		// 2.g
+		int age = scanner.nextInt();
+
+		System.out.println("You are " + age + " years old");
+
+		// 2.h
+
+		int retirement = 67 - age;
+
+		System.out.println("You have " + retirement + " years until retirement");
 
 	}
 
