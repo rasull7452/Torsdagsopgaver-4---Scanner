a71d5fb58b2c0d16844358002454e99f9c8eb28d
rasull7452 <rasullhossein250@gmail.com>
2025-09-25 18:31:08 +0200

    Task 3

--- Task 3/GuessANumber.java
+++ Task 3/GuessANumber.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;  
 
+// 3.a
+
 public class GuessANumber {
     private static int rnd_number;
 	
@@ -10,11 +12,38 @@ public class GuessANumber {
         rnd_number = random.nextInt(100) + 1;
         System.out.println( "I'm thinking of a number between 1 and 100 (including both).");
 	System.out.println( "Can you guess what it is?...");
+    System.out.println(rnd_number);
         makeAGuess();
     }
 
-
     private static void makeAGuess(){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        if(scanner.hasNextDouble() == true){
+            double input = scanner.nextDouble();
+            if(input == rnd_number){
+
+                System.out.println("You guessed " + rnd_number + " which is correct");
+
+            }else if(input < rnd_number){
+
+                System.out.println("Your guess was too low, try going higher");
+                makeAGuess();
+
+            }else if(input > rnd_number){
+
+                System.out.println("Your guess was too high, try going lower");
+                makeAGuess();
+
+            }
+        } else{
+
+                System.out.println("You may only type a number");
+                makeAGuess();
+
+            }
 	// Create a Scanner object   	 
         // use hasNextDouble to check if input is numeric, 
         // if so...
