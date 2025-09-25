ae1eb5600754ac1ae357674dad24c33618de639e
rasull7452 <rasullhossein250@gmail.com>
2025-09-25 15:24:22 +0200

    Task 1

---
+++ Task 1/Main.java
@@ -0,0 +1,9 @@
+// 1.a
+
+public class Main{
+
+	public static void main(String[] args){
+
+
+	}
+}
---
+++ Task 1/Team.java
@@ -0,0 +1,20 @@
+// 1.b
+
+public class Team{
+
+	// 1.c
+	private String teamName;
+
+	// 1.d
+	private int rank;
+
+	// 1.e
+	private ArrayList<String> members;
+
+		// 1.f
+		public Team(String teamName){
+
+			this.teamName = teamName;
+
+		}
+}
