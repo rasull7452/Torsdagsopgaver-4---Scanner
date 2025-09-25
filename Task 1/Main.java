a3474f981037225e15ea0081489ba09eacc3bcd3
rasull7452 <rasullhossein250@gmail.com>
2025-09-25 16:06:28 +0200

    Task 1

--- Task 1/Main.java
+++ Task 1/Main.java
@@ -4,6 +4,26 @@ public class Main{
 
 	public static void main(String[] args){
 
+		// 1.g
+		Team t1 = new Team("DD");
+
+		// 1.k
+		Team t2 = new Team("Blue");
+		Team t3 = new Team("Red");
+		Team t4 = new Team("Yellow");
+		Team t5 = new Team("Green");
+		Team t6 = new Team("Orange");
+
+		System.out.println(t2.toString());
+		System.out.println(t3.toString());
+		System.out.println(t4.toString());
+		System.out.println(t5.toString());
+		System.out.println(t6.toString());
+
+		t1.setRank(1);
+		t1.addPlayer("Igor");
+		t1.addPlayer("John");
+		System.out.println(t1.toString());
 
 	}
 }
--- Task 1/Team.java
+++ Task 1/Team.java
@@ -1,4 +1,5 @@
 // 1.b
+import java.util.ArrayList;
 
 public class Team{
 
@@ -9,12 +10,40 @@ public class Team{
 	private int rank;
 
 	// 1.e
-	private ArrayList<String> members;
+	private ArrayList<String> members = new ArrayList<String>();
 
 		// 1.f
 		public Team(String teamName){
 
 			this.teamName = teamName;
+
+		}
+
+		// 1.h
+		public void setRank(int newRank){
+
+			this.rank = newRank;
+
+
+		}
+
+		// 1.j
+		public String toString(){
+			String teamInfo = "Hold: " + this.teamName + " | Rang: " + this.rank;
+			String teamMembers = "\nMembers: \n";
+
+				// 1.m
+				for(String player : members){
+					teamMembers += player + " \n";
+				}
+
+			return teamInfo + " " + teamMembers;
+		}
+
+		// 1.l
+		public void addPlayer(String player){
+
+			this.members.add(player);
 
 		}
 }
